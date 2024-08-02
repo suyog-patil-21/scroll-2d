39055ca5e5194f16eae23aa4afd209553a43f509
Suyog Patil <suyog0057@gmail.com>
2024-08-02 14:44:21 +0530

    fix: change colorpalette colors

--- lib/src/utils/color_palette.dart
+++ lib/src/utils/color_palette.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 class ColorPalette {
   static const Color orange = Colors.orangeAccent;
-  static final Color orangeShade100 = Colors.orangeAccent.shade100;
-  static const Color blue = Colors.blueAccent;
+  static const Color orangeShade100 = Color(0xFFFFF8E3);
+  static const Color blue = Color(0xFF4044EE);
   static final Color blueShade100 = Colors.blueAccent.shade100;
 
   static const Color green = Colors.green;
   static const Color red = Colors.red;
 
-  static const Color lightGrey = Color(0xffc4c2cf);
-  static const Color lightGrey2 = Color(0xfff2f5f5);
-  static const Color lightGrey3 = Color(0xffebeded);
+  static const Color lightGrey = Color(0xFFE2E4E7);
+  static const Color lightGrey2 = Color(0xFFF8F8FC);
+  static const Color lightGrey3 = Color(0xFFF5F4F5);
 }
